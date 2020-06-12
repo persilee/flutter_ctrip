deea3b7b096cd9b38966f7d5655e41f5e0ad9f02
persilee <i@lishaoy.net>
2020-06-12 21:13:56 +0800

    🍊modify travel page

--- lib/pages/travel_tab_page.dart
+++ lib/pages/travel_tab_page.dart
@@ -1,12 +1,3 @@
-/**
- * 《Flutter从入门到进阶-实战携程网App》
- * 课程地址：
- * https://coding.imooc.com/class/321.html
- * 课程代码、文档：
- * https://git.imooc.com/coding-321/
- * 课程辅导答疑区：
- * http://coding.imooc.com/learn/qa/321.html
- */
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:flutter_ctrip/dao/travel_dao.dart';
